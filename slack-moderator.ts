c27283fb79b97f4fdd42efb4ae107a8c651c6683
quirimmo <quirimmo@gmail.com>
2017-04-08 16:33:34 +0100

    implemented base connection and removed share of slack.json config file with tokens

--- slack-moderator.ts
+++ slack-moderator.ts
@@ -3,3 +3,10 @@ import { SlackProxy } from "./src/SlackProxy";
 console.log('Slack Moderator Started!');
 
 let slackProxy: SlackProxy = new SlackProxy();
+slackProxy.initSlack().then(bootstrapApp);
+
+function bootstrapApp(data) {
+
+    console.log('Slack Moderator Fully Connected!');
+
+}
--- src/SlackProxy.ts
+++ src/SlackProxy.ts
@@ -3,7 +3,7 @@ import { SlackEvents } from './SlackEvents';
 
 declare function require(name: string);
 
-class SlackProxy implements SlackEvents{
+class SlackProxy implements SlackEvents {
 
     SLACK_CONFIG: any;
     moderators: Array<string>;
@@ -12,23 +12,43 @@ class SlackProxy implements SlackEvents{
     CLIENT_EVENTS: any;
     RTM_EVENTS: any;
     rtmClient: any;
+    webClient: any;
 
-    constructor() { 
+    constructor() {
         this.SLACK_CONFIG = Utils.getSlackConfig();
         this.moderators = this.SLACK_CONFIG.MODERATORS;
         this.inappropriateWords = this.SLACK_CONFIG.INAPPROPRIATE_WORDS;
     }
 
-    public initSlack(): void {
+    public initSlack(): Promise<any> {
         this.SLACK_CLIENT = require('@slack/client');
         this.CLIENT_EVENTS = this.SLACK_CLIENT.CLIENT_EVENTS;
         this.RTM_EVENTS = this.SLACK_CLIENT.RTM_EVENTS;
+        this.initSlackWebClient();
+        return this.initSlackRTMClient();
     }
+
+    private initSlackWebClient(): void {
+        let WebClient = this.SLACK_CLIENT.WebClient;
+        this.webClient = new WebClient(this.SLACK_CONFIG.TOKENS.API);
+    }
+
+    private initSlackRTMClient(): Promise<any> {
+        let RtmClient: any = this.SLACK_CLIENT.RtmClient;
+        console.log(this.SLACK_CONFIG.TOKENS.BOT);
+        this.rtmClient = new RtmClient(this.SLACK_CONFIG.TOKENS.BOT);
+        this.rtmClient.start();
+        let rtmInitPromise: Promise<any> = new Promise<any>((resolve, reject) => {
+            this.bindActionOnRTMConnectionOpened(resolve);
+        });
+        return rtmInitPromise;
+    }
+
 
     public bindActionOnRTMConnectionOpened(fnCallback: Function): void {
         this.rtmClient.on(this.CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, fnCallback);
     }
-    
+
 }
 
 export { SlackProxy };
