a0915a7880068a8e007c85363fc7bffcc327180f
quirimmo <quirimmo@gmail.com>
2017-04-09 01:05:09 +0100

    changed interface Channel to class Channel

--- slack-moderator.ts
+++ slack-moderator.ts
@@ -6,10 +6,10 @@ console.log('Slack Moderator Started!');
 // SlackChannel.getChannelsList().then((channels: any) => {
 //     console.log(channels);
 // });
-// SlackChannel.getChannelByID('C49K0TXPC').then((channel: any) => {
-//     let selectedChannel: Channel = channel;
-//     console.log(channel);
-// });
+SlackChannel.getChannelByID('C49K0TXPC').then((channel: any) => {
+    let selectedChannel: Channel = new Channel(channel.id, channel.name);
+    console.log(selectedChannel);
+});
 
 // SlackProxy.getInstance().getRTMPromise().then((data: any) => {
 //     console.log('Promise RTM Resolved');
--- src/Channel.ts
+++ src/Channel.ts
@@ -1,6 +1,11 @@
-interface Channel {
+class Channel {
     id: string;
     name: string;
+
+    constructor(id?: string, name?: string) {
+        if (id) this.id = id;
+        if (name) this.name = name;
+    }
 }
 
 export { Channel };
